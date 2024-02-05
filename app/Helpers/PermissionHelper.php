f40b2b7e6f71cb214f0f48b35bb888bea053c5d0
Tubagus Iksan <tebeiksan@gmail.com>
2024-02-05 14:50:11 +0700

    Add Role Policy

--- app/Helpers/PermissionHelper.php
+++ app/Helpers/PermissionHelper.php
@@ -10,4 +10,10 @@ class PermissionHelper
     const USER_VIEW = "USER_VIEW";
     const USER_SYNC_ROLE = "USER_SYNC_ROLE";
     const USER_SYNC_PERMISSION = "USER_SYNC_PERMISSION";
+    const ROLE_VIEW_ANY = "ROLE_VIEW_ANY";
+    const ROLE_VIEW = "ROLE_VIEW";
+    const ROLE_CREATE = "ROLE_CREATE";
+    const ROLE_UPDATE = "ROLE_UPDATE";
+    const ROLE_DELETE = "ROLE_DELETE";
+    const ROLE_SYNC_PERMISSION = "ROLE_SYNC_PERMISSION";
 }
